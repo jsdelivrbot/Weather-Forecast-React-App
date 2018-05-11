07bb146a1f51c480182dd7d9787d2ec4497613cc
Nicholas Ferrara <ferraranicholas@yahoo.com>
2018-05-10 23:14:53 -0400

    Sent search term to action creator

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = { term: ''}
@@ -9,6 +12,7 @@ export default class SearchBar extends Component {
         //Take the existing function and bind it to this, then replace it.
         //Eliminates the need for a fat arrow function
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -18,6 +22,9 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault();
+
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: ''});
     }
 
     render() {
@@ -35,3 +42,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators( {fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
